bac93f93a0b293eaaea5268268eb18f0c5a7c516
Andrew Tomaka <atomaka@gmail.com>
2014-10-15 13:41:47 -0400

    Add login filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:error] = 'You must be logged in to access this page'
+      redirect_to root_path
+    end
+  end
 end
