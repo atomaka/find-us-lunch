b3640e078c365af0f6dee7bf2c725debc6091ad3
Andrew Tomaka <atomaka@gmail.com>
2014-10-27 12:27:32 -0400

    Disallow self reference friend

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,11 @@ class Friendship < ActiveRecord::Base
   belongs_to :friend, class_name: 'User'
 
   validates_uniqueness_of :friend_id, :scope => :user_id
+  validate :disallow_self_reference
+
+  def disallow_self_reference
+    if friend_id == user_id
+      errors.add(:friend_id, 'cannot refer back to self')
+    end
+  end
 end
