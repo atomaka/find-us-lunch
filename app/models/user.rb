f592c80aaf6616f4b2cfa20c234c551a19a9e46c
Andrew Tomaka <atomaka@gmail.com>
2014-11-14 22:07:41 -0500

    Scope to not list self in users list

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :friendships
   has_many :friends, through: :friendships
 
+  scope :all_except, ->(user) { where.not(id: user) }
+
   def self.from_omniauth(auth)
     where(provider: auth[:provider], uid: auth[:uid]).first_or_create do |user|
       user.provider = auth[:provider]
